61d75814fc47a3bfd297bace2d888749ac961164
Horacio Moreno <yayin1111@gmail.com>
2018-11-20 20:52:35 -0500

    Add ArcadeDriveCommand

---
+++ FRC3620_2019_Doug_TrainingRobot/src/org/usfirst/frc3620/robot/commands/ArcadeDriveCommand.java
@@ -0,0 +1,48 @@
+package org.usfirst.frc3620.robot.commands;
+
+import java.sql.Driver;
+
+import org.usfirst.frc3620.robot.Robot;
+import org.usfirst.frc3620.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+/**
+ *
+ */
+public class ArcadeDriveCommand extends Command {
+	
+	DifferentialDrive differentialDrive = RobotMap.driveSubsystemDifferentialDrive;
+
+    public ArcadeDriveCommand() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.driveSubsystem);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	double vertical = Robot.oi.getLeftVerticalJoystick();
+    	double horizontal = Robot.oi.getLeftHorizontalJoystick();
+    	differentialDrive.arcadeDrive(vertical, horizontal);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
